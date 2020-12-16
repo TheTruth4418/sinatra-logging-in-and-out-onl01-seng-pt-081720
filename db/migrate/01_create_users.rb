b758ac83d881b8b75db8500494e32156887b9dea
William West <wwest466@gmail.com>
2020-12-16 18:39:10 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_users.rb
+++ db/migrate/01_create_users.rb
@@ -1,2 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :Users do |x|
+      x.string :username
+      x.string :password
+      x.float :balance
+    end
+  end
 end
